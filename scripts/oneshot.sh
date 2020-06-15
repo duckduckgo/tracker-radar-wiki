1ca0917583cb67f17da5701dd28b334173640956
Brad Slayter <slayterdevelopment@gmail.com>
2020-06-15 15:50:23 -0500

    Updated tracker template

--- scripts/oneshot.sh
+++ scripts/oneshot.sh
@@ -2,4 +2,4 @@
 
 source env.sh
 
-mustache -p ../templates/header.mustache -p ../templates/footer.mustache $TRACKER_RADAR_PATH/domains/google.com.json ../templates/tracker.mustache > ../docs/trackers/google.com.html
+mustache $TRACKER_RADAR_PATH/domains/google.com.json -p ../templates/header.mustache -p ../templates/footer.mustache ../templates/tracker.mustache > ../docs/trackers/google.com.html

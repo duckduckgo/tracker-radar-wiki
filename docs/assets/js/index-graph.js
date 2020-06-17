fbeba19c4947c8dbb44c4789ec6e72050d47d97d
Konrad Dzwinel <kdzwinel@gmail.com>
2020-06-17 20:29:02 +0200

    Show graphs with global trends on index page

---
+++ docs/assets/js/index-graph.js
@@ -0,0 +1,94 @@
+(function iife() {
+    const graph = document.querySelector('.graph-container__graph');
+    const controller = document.querySelector('.graph-container__controller');
+
+    if (!graph) {
+        console.warn('Missing .graph-container__graph element.');
+        return;
+    }
+
+    if (!window.wikiData || !window.wikiData.history || !window.wikiData.history.length) {
+        console.warn('Missing history data.');
+        return;
+    }
+
+    const colors = {
+        red: "rgb(255, 99, 132)",
+        orange: "rgb(255, 159, 64)",
+        yellow: "rgb(255, 205, 86)",
+        green: "rgb(75, 192, 192)",
+        blue: "rgb(54, 162, 235)",
+        purple: "rgb(153, 102, 255)",
+        grey: "rgb(201, 203, 207)"
+    };
+
+    // eslint-disable-next-line no-unused-vars, no-undef
+    const chart = new Chart(graph, {
+        type: 'line',
+        data: {
+            labels: window.wikiData.history.map(entry => entry.date),
+            datasets: [{
+                label: `# domains`,
+                backgroundColor: colors.red,
+                borderColor: colors.red,
+                data: window.wikiData.history.map(entry => entry.domains)
+            }]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 0
+                    }
+                }]
+            }
+        }
+    });
+
+    controller.addEventListener('change', () => {
+        if (controller.value === 'domains') {
+            chart.data.datasets = [{
+                label: `# domains`,
+                backgroundColor: colors.red,
+                borderColor: colors.red,
+                data: window.wikiData.history.map(entry => entry.domains)
+            }];
+        } else if (controller.value === 'entities') {
+            chart.data.datasets = [{
+                label: `# entities`,
+                backgroundColor: colors.blue,
+                borderColor: colors.blue,
+                data: window.wikiData.history.map(entry => entry.entities)
+            }];
+        } else if (controller.value === 'fingerprinting') {
+            chart.data.datasets = [{
+                label: `domains with score 0`,
+                borderColor: colors.green,
+                fill: false,
+                data: window.wikiData.history.map(entry => entry.fingerprinting[0])
+            },
+            {
+                label: `domains with score 1`,
+                borderColor: colors.yellow,
+                fill: false,
+                data: window.wikiData.history.map(entry => entry.fingerprinting[1])
+            },
+            {
+                label: `domains with score 2`,
+                borderColor: colors.orange,
+                fill: false,
+                data: window.wikiData.history.map(entry => entry.fingerprinting[2])
+            },
+            {
+                label: `domains with score 3`,
+                borderColor: colors.red,
+                fill: false,
+                data: window.wikiData.history.map(entry => entry.fingerprinting[3])
+            }];
+        }
+
+        chart.update();
+    });
+
+}());

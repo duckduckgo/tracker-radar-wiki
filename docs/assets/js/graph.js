1a1635b1715d89e34a7e83b542f93169bd212a0b
Konrad Dzwinel <kdzwinel@gmail.com>
2020-06-17 16:24:43 +0200

    Allow user to show graphs for fingerprinting, number of sites and cookies

--- docs/assets/js/graph.js
+++ docs/assets/js/graph.js
@@ -1,5 +1,6 @@
 (function iife() {
     const graph = document.querySelector('.graph-container__graph');
+    const controller = document.querySelector('.graph-container__controller');
 
     if (!graph) {
         console.warn('Missing .graph-container__graph element.');
@@ -11,6 +12,16 @@
         return;
     }
 
+    const colors = {
+        red: "rgb(255, 99, 132)",
+        orange: "rgb(255, 159, 64)",
+        yellow: "rgb(255, 205, 86)",
+        green: "rgb(75, 192, 192)",
+        blue: "rgb(54, 162, 235)",
+        purple: "rgb(153, 102, 255)",
+        grey: "rgb(201, 203, 207)"
+    };
+
     // eslint-disable-next-line no-unused-vars, no-undef
     const chart = new Chart(graph, {
         type: 'line',
@@ -18,8 +29,8 @@
             labels: window.wikiData.history.map(entry => entry.date),
             datasets: [{
                 label: `prevalence (%)`,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: colors.red,
+                borderColor: colors.red,
                 data: window.wikiData.history.map(entry => entry.prevalence * 100)
             }]
         },
@@ -34,6 +45,62 @@
                 }]
             }
         }
+    });
+
+    controller.addEventListener('change', () => {
+        if (controller.value === 'prevalence') {
+            chart.data.datasets = [{
+                label: `prevalence (%)`,
+                backgroundColor: colors.red,
+                borderColor: colors.red,
+                data: window.wikiData.history.map(entry => entry.prevalence * 100)
+            }];
+            chart.options.scales.yAxes = [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }];
+        } else if (controller.value === 'fingerprinting') {
+            chart.data.datasets = [{
+                label: `fingerprinting score`,
+                backgroundColor: colors.blue,
+                borderColor: colors.blue,
+                data: window.wikiData.history.map(entry => entry.fingerprinting)
+            }];
+            chart.options.scales.yAxes = [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 3,
+                    stepSize: 1
+                }
+            }];
+        } else if (controller.value === 'cookies') {
+            chart.data.datasets = [{
+                label: `cookies (%)`,
+                backgroundColor: colors.green,
+                borderColor: colors.green,
+                data: window.wikiData.history.map(entry => entry.cookies * 100)
+            }];
+            chart.options.scales.yAxes = [{
+                ticks: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            }];
+        } else if (controller.value === 'sites') {
+            chart.data.datasets = [{
+                label: `# of sites`,
+                backgroundColor: colors.orange,
+                borderColor: colors.orange,
+                data: window.wikiData.history.map(entry => entry.sites)
+            }];
+            chart.options.scales.yAxes = [{
+                ticks: {}
+            }];
+        }
+
+        chart.update();
     });
 
 }());
